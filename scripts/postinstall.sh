2d161bfef34538247f12505bb780d556c1c69b98
bott <bott@adobe.com>
2022-07-23 19:35:50 +0200

    minor changes

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -8,3 +8,6 @@ chmod -R 755  /usr/share/icons/elementary-breeze-dev
 chown -R root:root /usr/share/fonts/elementary-breeze
 chmod -R 755 /usr/share/fonts/elementary-breeze
 
+chown -R root:root /usr/share/QtCurve/Elementary-Breeze.qtcurve
+chmod -R 755 /usr/share/QtCurve/Elementary-Breeze.qtcurve
+
